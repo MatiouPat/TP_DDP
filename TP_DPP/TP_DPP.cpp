47b3d477c5aa897ecc85a8e51e7e2027e6c54d6d
Matthieu PAYS <pays.matthieuic@gmail.com>
2023-11-23 09:57:05 +0100

    4 - Rendre possible l'ajour de points dans les quatre directions possibles

--- TP_DPP/TP_DPP.cpp
+++ TP_DPP/TP_DPP.cpp
@@ -10,87 +10,147 @@ enum SquareState
 
 enum Orientation
 {
-    WEST = 0,
-    NORTH = 1,
-    EAST = 2,
-    SOUTH = 3
+    WEST = 1,
+    NORTH = 2,
+    NORTH_WEST = 3,
+    EAST = 4,
+    NORTH_EAST = 6,
+    SOUTH = 8,
+    SOUTH_WEST = 9,
+    SOUTH_EAST = 12
 };
 
-/**
-* Print
-*/
-void printChessboard(const std::deque<std::deque<SquareState>>& chessboard)
+class Board
 {
-    for (const auto& row : chessboard)
+public:
+    Board(int _sizeX, int _sizeY)
     {
-        for (const auto& square : row)
+        sizeX = _sizeX;
+        sizeY = _sizeY;
+        board = std::deque<std::deque<SquareState>>(_sizeY, std::deque<SquareState>(_sizeX, SquareState::UNKNOWN));
+        board[0][0] = SquareState::TRAVERSABLE;
+    }
+    /**
+    * 0-Left : 0001 / 1-Top : 0010 / 2-Right : 0100 / 3-Bottom : 1000
+    */
+    void addChessboardCase(Orientation orientation)
+    {
+        switch (orientation)
         {
-            char symbol;
-            switch (square)
-            {
-            case UNKNOWN:
-                symbol = '?';
-                break;
-            case TRAVERSABLE:
-                symbol = 'o';
-                break;
-            case UNTRAVERSABLE:
-                symbol = 'x';
-                break;
+        case WEST:
+        {
+            for (auto& row : board) {
+                row.push_front(SquareState::UNKNOWN);
             }
-            std::cout << symbol << ' ';
+            sizeX++;
+            break;
+        }
+        case NORTH:
+        {
+            std::deque<SquareState> newRow(sizeX, SquareState::UNKNOWN);
+            board.push_front(newRow);
+            sizeY++;
+            break;
+        }
+        case EAST:
+        {
+            for (auto& row : board) {
+                row.push_back(SquareState::UNKNOWN);
+            }
+            sizeX++;
+            break;
+        }
+        case SOUTH:
+        {
+            std::deque<SquareState> newRow(sizeX, SquareState::UNKNOWN);
+            board.push_front(newRow);
+            sizeY++;
+            break;
+        }
+        case NORTH_WEST:
+        {
+            for (auto& row : board) {
+                row.push_front(SquareState::UNKNOWN);
+            }
+            sizeX++;
+            std::deque<SquareState> newRow(sizeX, SquareState::UNKNOWN);
+            board.push_front(newRow);
+            sizeY++;
+            break;
+        }
+        case NORTH_EAST:
+        {
+            for (auto& row : board) {
+                row.push_back(SquareState::UNKNOWN);
+            }
+            sizeX++;
+            std::deque<SquareState> newRow(sizeX, SquareState::UNKNOWN);
+            board.push_front(newRow);
+            sizeY++;
+            break;
+        }
+        case SOUTH_WEST:
+        {
+            for (auto& row : board) {
+                row.push_front(SquareState::UNKNOWN);
+            }
+            sizeX++;
+            std::deque<SquareState> newRow(sizeX, SquareState::UNKNOWN);
+            board.push_front(newRow);
+            sizeY++;
+            break;
+        }
+        case SOUTH_EAST:
+        {
+            for (auto& row : board) {
+                row.push_back(SquareState::UNKNOWN);
+            }
+            sizeX++;
+            std::deque<SquareState> newRow(sizeX, SquareState::UNKNOWN);
+            board.push_front(newRow);
+            sizeY++;
+            break;
+        }
+        }
+    }
+    /**
+    * Print
+    */
+    void printChessboard()
+    {
+        for (const auto& row : board)
+        {
+            for (const auto& square : row)
+            {
+                char symbol;
+                switch (square)
+                {
+                case UNKNOWN:
+                    symbol = '?';
+                    break;
+                case TRAVERSABLE:
+                    symbol = 'o';
+                    break;
+                case UNTRAVERSABLE:
+                    symbol = 'x';
+                    break;
+                }
+                std::cout << symbol << ' ';
+            }
+            std::cout << '\n';
         }
         std::cout << '\n';
     }
-}
-
-/**
-* 0-Left / 1-Top / 2-Right / 3-Bottom
-*/
-void addChessboardCase(std::deque<std::deque<SquareState>>& chessboard, Orientation orientation, int& sizeX, int& sizeY)
-{
-    switch (orientation)
-    {
-    case WEST:
-    {
-        for(auto& row : chessboard) {
-            row.push_front(SquareState::UNKNOWN);
-        }
-        sizeX++;
-        break;
-    }
-    case NORTH:
-    {
-        std::deque<SquareState> newRow(sizeX, SquareState::UNKNOWN);
-        chessboard.push_front(newRow);
-        sizeY++;
-        break;
-    }
-    case EAST:
-    {
-        for (auto& row : chessboard) {
-            row.push_back(SquareState::UNKNOWN);
-        }
-        sizeX++;
-        break;
-    }
-    case SOUTH:
-    {
-        std::deque<SquareState> newRow(sizeX, SquareState::UNKNOWN);
-        chessboard.push_front(newRow);
-        sizeY++;
-        break;
-    }
-    }
-}
+private:
+    int sizeX;
+    int sizeY;
+    std::deque<std::deque<SquareState>> board;
+};
 
 int main()
 {
-    int sizeX = 10;
-    int sizeY = 10;
-    std::deque<std::deque<SquareState>> board(sizeY, std::deque<SquareState>(sizeX, SquareState::UNKNOWN));
-    board[0][0] = SquareState::TRAVERSABLE;
-    addChessboardCase(board, Orientation::WEST, sizeX, sizeY);
-    addChessboardCase(board, Orientation::NORTH, sizeX, sizeY);
-    printChessboard(board);
+    std::shared_ptr<Board> board = std::shared_ptr<Board>(new Board(10, 10));
+    board->printChessboard();
+    board->addChessboardCase(Orientation::NORTH_WEST);
+    board->printChessboard();
 }
